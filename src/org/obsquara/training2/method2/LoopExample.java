4d0870888321a2187daa68d751b7830acdd73067
Raji <Raji@LAPTOP-J7FA6U9I>
2024-02-23 19:41:38 +0530

    committed files

---
+++ src/org/obsquara/training2/method2/LoopExample.java
@@ -0,0 +1,10 @@
+package org.obsquara.training2.method2;
+
+public class LoopExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
